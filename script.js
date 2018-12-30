a4fd348524e4b60bf4032a22f5cfc231dcb47c1f
ytkimirtiapptasty <ytkimirti@apptastygames.com>
2018-12-30 15:49:01 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var map = L.map('mapid').setView([51.505,-0.9],13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoieXRraW1pcnRpIiwiYSI6ImNqcWFvczlleTBwNjE0Mm5raXk3bTB1a2gifQ.pP3T3gxQ_0Wd0hmaih1BcA'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
